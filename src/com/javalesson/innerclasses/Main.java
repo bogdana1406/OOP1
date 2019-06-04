e31ddf8db7db2e346aa3966d88621592774df47e
bogdana1406 <bogdana1406@gmail.com>
2019-06-04 16:01:23 +0300

    classes

---
+++ src/com/javalesson/innerclasses/Main.java
@@ -0,0 +1,13 @@
+package com.javalesson.innerclasses;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CellPhone phone = new CellPhone("Motorola", "XT1575");
+        phone.turnOn();
+
+        phone.call("1234567890");
+
+
+    }
+}
